b98c11ce37e7d2e2635f5c1f29faa5d33067109d
Tim Deve <timdeve@users.noreply.github.com>
2019-03-14 21:14:18 +0000

    Adds logging to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,7 @@ extern crate tower_web;
 extern crate chrono;
 #[macro_use]
 extern crate diesel;
+extern crate env_logger;
 extern crate http;
 extern crate rss;
 extern crate tokio;
@@ -14,15 +15,19 @@ pub mod stories;
 
 use feeds::FeedsResource;
 use stories::StoriesResource;
+use tower_web::middleware::log::LogMiddleware;
 use tower_web::ServiceBuilder;
 
 pub fn main() {
+    let _ = env_logger::try_init();
+
     let addr = "127.0.0.1:8091".parse().expect("Invalid address");
     println!("Listening on http://{}", addr);
 
     ServiceBuilder::new()
         .resource(FeedsResource)
         .resource(StoriesResource)
+        .middleware(LogMiddleware::new("rasasa-server::web"))
         .run(&addr)
         .unwrap();
 }
