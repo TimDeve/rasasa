2f66d9669a2149d7009fc875fd376e72cd9fcdbc
Tim Deve <timdeve@users.noreply.github.com>
2019-04-02 18:39:01 +0100

    Adds basic manifest.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
@@ -72,5 +73,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve('src/index.html') }),
     new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin([{from: 'static'}])
   ],
 }
