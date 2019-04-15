29663f256f521f17998263facd0e62bd45c9593c
Tim Dévé <tdeve@thoughtworks.com>
2019-04-15 19:03:07 +0100

    Adds backgroundSync for PATCH requests

--- client/src/sw.js
+++ client/src/sw.js
@@ -2,6 +2,18 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox
 
 if (workbox) {
   workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
+  const bgSyncPlugin = new workbox.backgroundSync.Plugin('updateQueue', {
+    maxRetentionTime: 48 * 60,
+  })
+
+  workbox.routing.registerRoute(
+    /\/api\/v0\/.+/,
+    new workbox.strategies.NetworkOnly({
+      plugins: [bgSyncPlugin],
+    }),
+    'PATCH'
+  )
 } else {
   console.error('Workbox failed to load.')
 }
