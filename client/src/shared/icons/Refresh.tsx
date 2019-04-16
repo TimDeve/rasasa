aaafc4604088cf49103aaf7d38020f070dcfd9bb
Tim Deve <timdeve@users.noreply.github.com>
2019-04-16 18:31:44 +0100

    Adds refresh icon

---
+++ client/src/shared/icons/Refresh.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import cn from 'classnames'
+
+import s from './Refresh.scss'
+
+interface RefreshProps extends React.HTMLProps<SVGSVGElement> {
+  color?: string
+  animated?: boolean
+}
+
+function Refresh({ color, animated, ...rest }: RefreshProps) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
+      <path
+        className={cn({ [s.animatedPath]: animated })}
+        d="M16 20C11.3137 20 4 20 4 20V4H20V18.5L16 15"
+        stroke={color}
+        strokeMiterlimit="10"
+      />
+    </svg>
+  )
+}
+
+Refresh.defaultProps = {
+  color: '#000',
+}
+
+export default Refresh
