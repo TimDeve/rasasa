07c925144895b9b6ddfb721fbe8340a945f4b1d8
Tim Dévé <tdeve@thoughtworks.com>
2019-04-17 16:58:05 +0100

    Adds a loading state to the Button component

--- client/src/shared/components/Button.tsx
+++ client/src/shared/components/Button.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import cn from 'classnames'
 
 import s from './Button.scss'
+import Refresh from 'shared/icons/Refresh'
 
-function Button(props: React.HTMLProps<HTMLButtonElement>) {
-  return <button {...props} className={cn(s.component, props.className)} />
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+  loading?: boolean
+}
+
+function Button({loading, className,children, ...rest}:ButtonProps) {
+  return (
+    <button {...rest} className={cn(s.component, className, { [s.componentLoading]: loading })}>
+      {children}
+      {loading && <Refresh animated className={s.loadingIndicator}/>}
+    </button>
+  )
 }
 
 export default Button
