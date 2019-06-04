4839ee529e99fbeac227865605ab8b67971b9750
Tim Dévé <tdeve@thoughtworks.com>
2019-06-04 13:24:57 +0100

    Adds login endpoint

--- gateway/main.go
+++ gateway/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 
+	"github.com/go-session/redis"
+	"github.com/go-session/session"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 )
+
+var adminUsername = os.Getenv("RASASA_USER")
+var adminPassword = os.Getenv("RASASA_PASS")
+
+var adminLogin = login{
+	Username: &adminUsername,
+	Password: &adminPassword,
+}
 
 var rasasaServer = "http://localhost:8091"
 var readServer = "http://localhost:8092"
 
 func main() {
+	initSession()
+
 	r := mux.NewRouter()
+
+	r.HandleFunc("/api/v0/login", loginHandler).Methods("POST")
 
 	r.HandleFunc("/api/{proxyPath:v0/read.*}", proxy(readServer))
 	r.HandleFunc("/api/{proxyPath:.*}", proxy(rasasaServer))
@@ -26,8 +43,36 @@ func main() {
 	n := negroni.Classic()
 	n.UseHandler(r)
 
-	fmt.Println("Listening on 8090")
-	http.ListenAndServe(":8090", n)
+	fmt.Println("Listening on localhost:8090")
+	http.ListenAndServe("localhost:8090", n)
+}
+
+func loginHandler(wr http.ResponseWriter, req *http.Request) {
+	store, err := session.Start(context.Background(), wr, req)
+	if err != nil {
+		http.Error(wr, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	d := json.NewDecoder(req.Body)
+	d.DisallowUnknownFields()
+
+	l := login{}
+	err = d.Decode(&l)
+	if err != nil {
+		http.Error(wr, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if !l.Equal(adminLogin) {
+		http.Error(wr, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	store.Set("authenticated", true)
+	if store.Save() != nil {
+		http.Error(wr, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	fmt.Fprint(wr, http.StatusText(http.StatusOK))
 }
 
 func proxy(target string) func(http.ResponseWriter, *http.Request) {
@@ -38,8 +83,26 @@ func proxy(target string) func(http.ResponseWriter, *http.Request) {
 
 	proxy := httputil.NewSingleHostReverseProxy(url)
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		r.URL.Path = mux.Vars(r)["proxyPath"]
-		proxy.ServeHTTP(w, r)
+	return func(wr http.ResponseWriter, req *http.Request) {
+		req.URL.Path = mux.Vars(req)["proxyPath"]
+		proxy.ServeHTTP(wr, req)
 	}
+}
+
+func initSession() {
+	session.InitManager(
+		session.SetStore(redis.NewRedisStore(&redis.Options{
+			Addr: os.Getenv("REDIS_URL"),
+			DB:   1,
+		})),
+	)
+}
+
+type login struct {
+	Username *string `json:"username"`
+	Password *string `json:"password"`
+}
+
+func (aLogin login) Equal(anotherLogin login) bool {
+	return *aLogin.Username == *anotherLogin.Username && *aLogin.Password == *anotherLogin.Password
 }
