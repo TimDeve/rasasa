2beb250371edbe8b4ee5828b76e119f753143483
Tim Dévé <tdeve@thoughtworks.com>
2019-06-05 15:56:17 +0100

    Makes read server logs pretty

--- read-server/src/index.js
+++ read-server/src/index.js
@@ -15,7 +15,7 @@ const REDIS_PREFIX = 'rasasa-read'
 const redisClient = redis.createClient(process.env['REDIS_URL'])
 
 const fastify = require('fastify')({
-  logger: true,
+  logger: { prettyPrint: true },
 })
 
 function prefixPageCaching(pageUrl) {
