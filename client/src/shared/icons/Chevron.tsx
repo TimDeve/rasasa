7445f59d799e85492d3ac3a92882996b0b74bc03
Tim Dévé <timdeve@users.noreply.github.com>
2019-12-26 17:37:44 +0100

    Animates chevon when changing direction

--- client/src/shared/icons/Chevron.tsx
+++ client/src/shared/icons/Chevron.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import s from './Chevron.scss'
+
 type Direction = 'up' | 'down' | 'flat'
 
 interface ChevronProps extends React.HTMLProps<SVGSVGElement> {
@@ -10,19 +12,19 @@ interface ChevronProps extends React.HTMLProps<SVGSVGElement> {
 function Chevron({ color, direction, ...rest }: ChevronProps) {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
-      <path d={generatePath(direction)} stroke={color} />
+      <path className={`${s.path} ${generatePath(direction)}`} stroke={color} />
     </svg>
   )
 }
 
 function generatePath(direction?: Direction): string {
   switch (direction) {
-    case 'down':
-      return 'M4 7L12 17L20 7'
+    case 'up':
+      return s.up
     case 'flat':
-      return 'M4 12L12 12L20 12'
+      return s.flat
     default:
-      return 'M4 17L12 7L20 17'
+      return s.down
   }
 }
 
