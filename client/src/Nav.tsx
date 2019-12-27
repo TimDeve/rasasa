b77c00a131633f710f5fc2d7b34e1604c3d8355e
Tim Dévé <timdeve@users.noreply.github.com>
2019-12-27 13:30:19 +0100

    Adds icon to Nav items

--- client/src/Nav.tsx
+++ client/src/Nav.tsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import { LoggedInContext, LoggedInState } from 'shared/loggedInState/LoggedInContext'
+import ListIcon from 'shared/icons/ListIcon'
+import FeedIcon from 'shared/icons/FeedIcon'
+import ConnectionIcon from 'shared/icons/ConnectionIcon'
 import s from './Nav.scss'
 
 function Nav() {
@@ -11,21 +14,21 @@ function Nav() {
     <nav className={s.nav}>
       <ul>
         <li>
-          <Link to="/lists">Lists</Link>
+          <Link to="/lists"><ListIcon/> Lists</Link>
         </li>
         <li>
-          <Link to="/feeds">Feeds</Link>
+          <Link to="/feeds"><FeedIcon/> Feeds</Link>
         </li>
       </ul>
       <ul>
         {loggedInState === LoggedInState.LoggedOut && (
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/login"><ConnectionIcon/> Login</Link>
           </li>
         )}
         {loggedInState === LoggedInState.Offline && (
           <li>
-            <span className={s.disabled}>Offline</span>
+            <span className={s.disabled}><ConnectionIcon color='grey' /> Offline</span>
           </li>
         )}
       </ul>
