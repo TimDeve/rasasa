b3b2450627d08f26bf592c065e95dd659a581da8
Tim Deve <timdeve@users.noreply.github.com>
2019-12-29 12:01:47 +0000

    Specifies `d` prop both in css and html in Chevron component
    Fixes regression introduced in 7445f59 where in non-Blink based browser
    the chevron icon was not visible, caused by the fact that the d property
    can only be set via css in Blink based browsers

--- client/src/shared/icons/ChevronIcon.tsx
+++ client/src/shared/icons/ChevronIcon.tsx
@@ -10,21 +10,23 @@ interface ChevronProps extends React.HTMLProps<SVGSVGElement> {
 }
 
 function Chevron({ color = '#000', direction = 'down', ...rest }: ChevronProps) {
+  const [className, pathDraw] = generatePath(direction)
+
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
-      <path className={`${s.path} ${generatePath(direction)}`} stroke={color} />
+      <path className={`${s.path} ${className}`} d={pathDraw} stroke={color} />
     </svg>
   )
 }
 
-function generatePath(direction?: Direction): string {
+function generatePath(direction?: Direction): [string, string] {
   switch (direction) {
     case 'up':
-      return s.up
+      return [s.up, 'M4 17L12 7L20 17']
     case 'flat':
-      return s.flat
+      return [s.flat, 'M4 12L12 12L20 12']
     default:
-      return s.down
+      return [s.down, 'M4 7L12 17L20 7']
   }
 }
 
