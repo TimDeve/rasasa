cf101f22e245ab5cca9c2f91673ca42ceb478500
Tim Dévé <TimDeve@users.noreply.github.com>
2025-11-14 23:52:42 +0000

    Send proper user-agent by default

--- read-server/src/transform/transformers.js
+++ read-server/src/transform/transformers.js
@@ -17,7 +17,7 @@ export function transformUserAgent(url) {
   return paywalledDomains.has(parsedUrl.host)
     // Pretend to be browser/googlebot
     ? 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.119 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-    : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'
+    : 'Rasasa read-server'
 }
 
 export function transformUrl(u) {
